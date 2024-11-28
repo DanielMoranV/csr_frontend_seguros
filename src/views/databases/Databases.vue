d2aeb40ed7e85e75e9e957bc0cf2e9065203cb1b
Daniel Moran Vilchez <skaan.dmv@gmail.com>
2024-11-28 17:47:15 -0500

    creacion de tablas y migraciones

--- src/views/databases/Databases.vue
+++ src/views/databases/Databases.vue
@@ -1,3 +1,39 @@
+<script setup>
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+const value = ref(0);
+let interval = null;
+
+function startProgress() {
+    interval = setInterval(() => {
+        let newValue = value.value + Math.floor(Math.random() * 10) + 1;
+        if (newValue >= 100) {
+            newValue = 100;
+        }
+        value.value = newValue;
+    }, 2000);
+}
+
+function endProgress() {
+    clearInterval(interval);
+    interval = null;
+}
+
+onMounted(() => {
+    startProgress();
+});
+
+onBeforeUnmount(() => {
+    endProgress();
+});
+</script>
 <template>
-    <h1>Bases de datos</h1>
+    <div class="card">
+        <div class="font-semibold text-xl mb-4">ProgressBar</div>
+        <div class="flex flex-col md:flex-row gap-4">
+            <div class="md:w-1/2">
+                <ProgressBar :value="value"></ProgressBar>
+            </div>
+        </div>
+    </div>
 </template>
